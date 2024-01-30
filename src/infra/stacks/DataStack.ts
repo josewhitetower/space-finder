0d92be26d63add2b803fbe76c4e8c443c3fa0b62
Jose Torreblanca <jose.torreblanca@moia.io>
2024-01-30 20:52:48 +0100

    Update API endpoint and integrate with DynamoDB table

--- src/infra/stacks/DataStack.ts
+++ src/infra/stacks/DataStack.ts
@@ -1,8 +1,23 @@
 import {Construct} from 'constructs'
 import {Stack, StackProps} from 'aws-cdk-lib'
+import { AttributeType, ITable, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { getSuffixFromStack } from '../Utils';
 
 export class DataStack extends Stack {
+    public readonly spacesTable: ITable
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
+        const suffix = getSuffixFromStack(this);
+
+        // create a dynameodb table
+        const table = new Table(this, 'SpacesTable', {
+            partitionKey: {
+                name: 'id',
+                type: AttributeType.STRING
+            },
+            tableName: `SpacesTable-${suffix}`
+
+        })
+        this.spacesTable = table
     }
 }
--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -3,16 +3,23 @@ import {Stack, StackProps} from 'aws-cdk-lib'
 import {Function as LambdaFunction, Runtime, Code} from 'aws-cdk-lib/aws-lambda'
 import { join } from 'path';
 import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
+import { ITable } from 'aws-cdk-lib/aws-dynamodb';
+interface LambdaStackProps extends StackProps {
+    spacesTable: ITable
+}
 
 export class LambdaStack extends Stack {
     public readonly helloLambdaIntegration: LambdaIntegration
-    constructor(scope: Construct, id: string, props?: StackProps) {
+    constructor(scope: Construct, id: string, props: LambdaStackProps) {
         super(scope, id, props);
 
         const helloLambda = new LambdaFunction(this, 'HelloLambda', {
             runtime: Runtime.NODEJS_LATEST,
             code: Code.fromAsset(join(__dirname, '..','..', 'services')),
-            handler: 'hello.main'
+            handler: 'hello.main',
+            environment: {
+                TABLE_NAME: props.spacesTable.tableName
+            }
         })
 
         this.helloLambdaIntegration = new LambdaIntegration(helloLambda)
