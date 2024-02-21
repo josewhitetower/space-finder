fed50c9c2a0866bf04ce54eb8a56c0765e4c417c
Jose Torreblanca <jose.torreblanca@moia.io>
2024-02-21 17:05:35 +0100

    Update launch.json, package.json, spaces.http, and test/launcher.ts

--- test/launcher.ts
+++ test/launcher.ts
@@ -1,3 +1,13 @@
 import { handler } from "../src/services/spaces/handler";
 
-handler({} as any, {} as any);
+// "AWS_REGION": "eu-central-1",
+// "TABLE_NAME": "SpacesTable-068cb2d50d41"
+process.env.TABLE_NAME = "SpacesTable-068cb2d50d41";
+process.env.AWS_REGION = "eu-central-1";
+
+handler({
+    httpMethod: "POST",
+    body: JSON.stringify({
+        location: 'Hamburg 3',
+    })
+} as any, {} as any);
