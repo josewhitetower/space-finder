0dfb929a88c1c85d4a2679e206e02729eb7116b3
Jose Torreblanca <jose.torreblanca@moia.io>
2024-03-01 20:34:16 +0100

    Add AWS SDK dependencies and update AuthService with temporary credentials generation

--- test/AuthService.ts
+++ test/AuthService.ts
@@ -1,4 +1,6 @@
 import { fetchAuthSession, getCurrentUser, signIn, signOut } from "@aws-amplify/auth";
+import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
+import { fromCognitoIdentity, fromCognitoIdentityPool } from "@aws-sdk/credential-providers";
 import { Amplify } from "aws-amplify";
 
 Amplify.configure({
@@ -6,6 +8,7 @@ Amplify.configure({
         Cognito: {
             userPoolId: 'eu-central-1_gis1IUKuD',
             userPoolClientId: 'iqaca37aersnimt07e7op2rqu',
+            identityPoolId: 'eu-central-1:bf412efa-964b-4000-932b-60ee8573bf54'
         },
     }
 })
@@ -19,5 +22,16 @@ export class AuthService {
         });
         const jwtToken = (await fetchAuthSession()).tokens.idToken.toString()
         return { result, jwtToken };
+    }
+
+    public async generateTemporaryCredentials(jwtToken: string) {
+        const credentials = fromCognitoIdentityPool({
+            identityPoolId: 'eu-central-1:bf412efa-964b-4000-932b-60ee8573bf54',
+            logins: {
+                'cognito-idp.eu-central-1.amazonaws.com/eu-central-1_gis1IUKuD': jwtToken
+            }
+        });
+        return credentials();
+
     }
 }
